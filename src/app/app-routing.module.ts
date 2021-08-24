748818b66d10188f97d1befbc9f09faf6da850e3
Kraft Benjamin (059) <>
2021-08-24 20:58:09 +0200

    added routes to cart and product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductItemDetailsComponent } from './product-list/product-item-details/product-item-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'product/:id', component: ProductItemDetailsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
